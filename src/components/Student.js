64a86f982a5453fcbc7740e3bd5feae56e36d6ee
mithun <mithunweerasinghe999@gmail.com>
2024-05-29 01:22:48 +0530

    finished adding student

--- src/components/Student.js
+++ src/components/Student.js
@@ -2,14 +2,30 @@ import * as React from 'react';
 import { useState } from 'react';
 import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
-import { Container, Paper } from '@mui/material';
+import { Container, Paper, Button } from '@mui/material';
+//import {SendIcon} from '@mui/icons-material';
 
 export default function Student() {
 
     const paperStyle={padding:'50px 20px', width:600, margin:'20px auto'};
     // const[name,setName]=useState('mithun'); will store the value with the form
     const[name,setName]=useState('');
-    const[address,setAddress]=useState('')
+    const[address,setAddress]=useState('');
+
+    const handleClick=(e)=>{
+        e.preventDefault()
+        const student={name,address}
+        console.log(student)
+
+        fetch("http://localhost:8080/student/add",{
+            method:"POST",
+            headers:{"Content-Type":"application/json"},
+            body:JSON.stringify(student)
+
+        }).then(()=>{
+            console.log("New Student added")
+        })
+    }
 
   return (
     <Container>
@@ -32,6 +48,8 @@ export default function Student() {
       value={address}
       onChange={(e)=>setAddress(e.target.value)}
       />
+
+       <Button variant="contained" onClick={handleClick} >Submit</Button>
       
     </Box>
     </Paper>
